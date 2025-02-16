37bfe29efa3e6393d236f3318cc44dcb0e118144
SNelson723 <stephenn649@gmail.com>
2025-02-16 15:32:37 -0600

    Finished the first starter sample challenge and all is working properly

--- src/projects/Sample1.tsx
+++ src/projects/Sample1.tsx
@@ -7,9 +7,23 @@ interface PokeDetails {
   // and any other fields I want to use
 }
 
+interface Poke {
+  base_experience: number;
+  weight: number;
+  height: number;
+  types: {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }[];
+}
+
 const Sample1 = () => {
   const url: string = "https://pokeapi.co/api/v2/pokemon/";
   const [pokemon, setPokemon] = useState<PokeDetails[]>([]);
+  const [details, setDetails] = useState<Poke | null>(null);
 
   useEffect(() => {
     axios
@@ -20,22 +34,42 @@ const Sample1 = () => {
       .catch((err) => console.log(err));
   }, [url]);
 
-  console.log(pokemon)
-
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log(e.currentTarget.value)
+    if (e.currentTarget.value) {
+      axios
+        .get(e.currentTarget.value)
+        .then(({ data }) => {
+          setDetails(data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setDetails(null)
+    }
   };
 
   return (
     <div className="bg-slate-600 mt-36 w-full flex justify-center items-center">
       <div className="text-slate-100">
+        <div className="text-center mb-2">Select a Pokemon</div>
         <div className="text-slate-900">
-          <select onChange={handleChange}>
+          <select className="w-[150px]" onChange={handleChange}>
             <option></option>
             {pokemon.map((poke: PokeDetails, i: number) => (
-              <option key={`pokemon_${i}`} value={poke.url}>{poke.name}</option>
+              <option key={`pokemon_${i}`} value={poke.url}>
+                {poke.name}
+              </option>
             ))}
           </select>
+        </div>
+        <div className="mt-4">
+          {details ? (
+            <div>
+              <div>Height :<span>{details.height}</span></div>
+              <div>Weight :<span>{details.weight}</span></div>
+              <div>Base Experience: <span>{details.base_experience}</span></div>
+              <div>Type: <span>{details.types[0].type.name}</span></div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
